1a5c888c06cb61d817dde2c43d50df021166d7c8
hongteck <hongteck24@gmail.com>
2022-07-12 17:49:42 +0800

    hongteck

--- app/src/main/java/example/asdasdasd123/MainActivity.kt
+++ app/src/main/java/example/asdasdasd123/MainActivity.kt
@@ -3,6 +3,7 @@ package example.asdasdasd123
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -20,5 +21,20 @@ class MainActivity : AppCompatActivity() {
         val randomNum = (1..6).random()
         val numTxt: TextView = findViewById(R.id.numberTxt)
         numTxt.text = randomNum.toString()
+
+        val diceImage : ImageView = findViewById(R.id.diceImg)
+        val imgSrc = when (randomNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(imgSrc)
+
+        Toast.makeText(this,randomNum.toString(),
+        Toast.LENGTH_SHORT).show()
     }
 }
