b0ea2a9134504fc7ff376b06036f49b97fe57f3c
kelperfish <nghorngping@gmail.com>
2022-07-19 17:55:16 +0800

    20220711 added text view added edit text

--- app/src/main/java/example/asdasdasd123/MainActivity.kt
+++ app/src/main/java/example/asdasdasd123/MainActivity.kt
@@ -1,28 +1,49 @@
 package example.asdasdasd123
 
+import android.content.Context
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView
+    lateinit var numbText: TextView
+    lateinit var nameTxt: EditText
+    lateinit var playerNameTxt: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        diceImg = findViewById(R.id.diceImg)
+        numbText = findViewById(R.id.numberTxt)
+        nameTxt = findViewById(R.id.editPlayerNameET)
+        playerNameTxt = findViewById(R.id.playerName)
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+        val updateBtn: Button = findViewById(R.id.updateButton)
+        updateBtn.setOnClickListener { updateName(it) }
+    }
+
+    private fun updateName(view: View){
+        playerNameTxt.text = nameTxt.text
+        nameTxt.text.clear()
+        nameTxt.clearFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun rollDice() {
         Toast.makeText(this, "Roll", Toast.LENGTH_SHORT).show()
         val randomNum = (1..6).random()
-        val numTxt: TextView = findViewById(R.id.numberTxt)
-        numTxt.text = randomNum.toString()
+//        val numTxt: TextView = findViewById(R.id.numberTxt)
+        numbText.text = randomNum.toString()
 
-        val diceImage : ImageView = findViewById(R.id.diceImg)
+//        val diceImage : ImageView = findViewById(R.id.diceImg)
         val imgSrc = when (randomNum){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -32,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(imgSrc)
+        diceImg.setImageResource(imgSrc)
 
         Toast.makeText(this,randomNum.toString(),
         Toast.LENGTH_SHORT).show()
